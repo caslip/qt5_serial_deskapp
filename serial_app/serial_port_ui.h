24c6a24cf61c863a44371fa37d2bae6cb6c2714f
Ubuntu_TY <359335314@qq.com>
2024-10-22 00:08:36 +0800

    串口头文件

--- serial_app/serial_port_ui.h
+++ serial_app/serial_port_ui.h
@@ -2,6 +2,9 @@
 #define SERIAL_PORT_UI_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QSerialPort>
+#include <QTextBrowser>
 
 namespace Ui {
 class Serial_port_ui;
@@ -15,8 +18,29 @@ public:
     explicit Serial_port_ui(QWidget *parent = nullptr);
     ~Serial_port_ui();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_btn_open_serial_clicked();
+
+    void on_to_home_btn_clicked();
+
+    void on_btn_clear_clicked();
+
+    void readbuf();
+
+    void on_btn_close_serial_clicked();
+
+    void on_btn_send_clicked();
+
 private:
     Ui::Serial_port_ui *ui;
+    QSerialPort *Serialport;
+
+    void ScanSerialport();
+    void InitCombox();
+
+
 };
 
 #endif // SERIAL_PORT_UI_H
