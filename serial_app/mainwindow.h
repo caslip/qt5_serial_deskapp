257c5ff9527e2508aa34c91e652807b44bbbb88b
Ubuntu_TY <359335314@qq.com>
2024-10-22 00:07:19 +0800

    主界面头文件

--- serial_app/mainwindow.h
+++ serial_app/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+
+
+private slots:
+    void on_pushButton_clicked();
 };
 #endif // MAINWINDOW_H
